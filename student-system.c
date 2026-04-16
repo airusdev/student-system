902a44f60e0109918919f39259aa026d070d0ab1
airusdev <work.airus@gmail.com>
2026-04-16 20:31:46 +0800

    added a placeholder menu - add student, view students, and exit

--- student-system.c
+++ student-system.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main() {
-    // code here
+    bool accepted_value = false; 
+    int choice;
+    
+    while (1) {
+        if (choice == 1) {
+            printf("placeholder for add_student()\n\n");
+        } else if (choice == 2) {
+            printf("placeholder for view students\n\n");
+        } else if (choice == 3) {
+            printf("Goodbye!\n\n");
+            break;
+        }
+        
+        accepted_value = false;
+        while (accepted_value == false) {
+            printf("Options:\n (1) Add Student\n (2) View Students\n (3) Exit\n\n");
+            scanf("%d", &choice);
 
+            if (choice == 1 || choice == 2 || choice == 3) {
+                accepted_value = true;
+            }
+        }
+    }
     return 0;
 }
