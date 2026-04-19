ae934b2de1ffa9af51319f22892a98b5a285526e
airusdev <work.airus@gmail.com>
2026-04-19 17:27:15 +0800

    menu input validation

--- student-system.c
+++ student-system.c
@@ -1,3 +1,5 @@
+// to add: input validation on the (1) (2) (3), input must be a number
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -25,20 +27,35 @@ void print_student(int index) {
 
 int main() {
 	bool accepted_value = false;
-	int choice = 0;
+    char choice[64] = {0};
+    int valid_answers[] = {'1', '2', '3'};
+	//int choice = 0;
 
 	while (1) {
         accepted_value = false; // wait until valid choice
         while (!accepted_value) {
             printf("Options:\n (1) Add Student\n (2) View Students\n (3) Exit\n\n");
-            scanf(" %d", &choice);
+            scanf(" %[^\n]%*c", choice);
 
-            if (choice == 1 || choice == 2 || choice == 3) {
-                accepted_value = true;
+            if (strlen(choice) > 1) {
+                printf("INVALID INPUT!\n\n");
+                continue;
+            }
+
+            for (int i = 0; i < (sizeof(choice) / sizeof(choice[0])); i++) {
+                if (choice[0] == valid_answers[i]) {
+                    accepted_value = true;
+                }
+            }
+
+            if (accepted_value == true) {
+                printf("VALID INPUT!!\n\n");
+            } else {
+                printf("INVALID INPUT!!\n\n");
             }
         }
 
-		if (choice == 1) { // we need to do an if condition, we have to make sure student_name_given has a value first
+		if (choice[0] == '1') { // we need to do an if condition, we have to make sure student_name_given has a value first
             char student_name_given[100];
             
             printf("Enter student name: \n");
@@ -46,20 +63,20 @@ int main() {
 
 			add_student(student_name_given);
 
-		} else if (choice == 2) {
+		} else if (choice[0] == '2') {
             int index = 0;
             printf("Please give the index of the student's name you want to print: ");
             scanf(" %d", &index);
             print_student(index - 1);
 
-			//printf("placeholder for view students\n\n");    
-		} else if (choice == 3) {
+			//printf("placeholder for view students\n\n");
+		} else if (choice[0] == '3') {
 			printf("Goodbye!\n\n");
 			break;
 		}
-		choice = 0;
 	}
 
+    choice[0] = '\0';
     for (int i = 0; i < student_counter; i++) {
         free(saved_student_names[i]);
     }
